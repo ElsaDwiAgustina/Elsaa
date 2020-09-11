ad11bad33b42c691c8427ea2e4893f376a6c2490
ElsaDwiAgustina <71110581+ElsaDwiAgustina@users.noreply.github.com>
2020-09-11 09:19:04 +0700

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,35 @@
+
+package utsJava;
+
+public class Java {
+    public static void main (String[] args ){
+        String var1 =" ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        char[] c = {var1.charAt(5),
+        var1.charAt(12),
+        var1.charAt(19),
+        var1.charAt(1),
+        var1.charAt(0),
+        var1.charAt(4),
+        var1.charAt(23),
+        var1.charAt(9),
+        var1.charAt(0),
+        var1.charAt(1),
+        var1.charAt(7),
+        var1.charAt(21),
+        var1.charAt(19),
+        var1.charAt(20),
+        var1.charAt(9),
+        var1.charAt(14),
+        var1.charAt(1),
+         
+        };
+                
+        for (int i=0;i<c.length;i++){
+        System.out.print(c[i]);
+        
+        }
+        System.out.println();
+
+    }
+        
+}
